326f2414f9bcba3b221cf265c4794b1df6e4fdb1
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2024-12-24 23:34:53 +0200

    add

--- src/main/java/com/javatask/jpa/Models/Auther.java
+++ src/main/java/com/javatask/jpa/Models/Auther.java
@@ -1,35 +1,23 @@
 package com.javatask.jpa.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-// i use lombok when add project
+// i use lombok when add project Data Containe=> Getter , Setter
+@NoArgsConstructor
+@Data
 @Entity
-@Getter
-@Setter
 public class Auther {
 
     @Id
+    @GeneratedValue
     private Integer id;
     private String firstName;
     private String lastName;
     private String email;
     private int age;
-
-    
-    public Auther() {
-    }
-
-
-    public Auther(String firestName, String lastName, String email, int age) {
-        this.firstName = firestName;
-        this.lastName = lastName;
-        this.email = email;
-        this.age = age;
-    }
-
- 
 
 }
